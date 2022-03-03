801094bf51d688595b98b46943689dae8d04c741
Shawn Schmalz <shawns@nittanyweb.com>
2022-03-03 18:37:53 +0000

    working on photoswipe

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,170 @@
 <template>
-    <p>Home</p>
+<div>
+    <div class="container primary">
+<section class="wrapper">
+
+<div class="flex_wrap">
+<div class="flex_4 flex_sub">
+<div class="flex_3 flex_3_no_100"><nuxt-img class="home_logo" src="/logo.png" width="123" height="124" alt="19th Century Paintings"/></div>
+<div class="flex_7 flex_center flex_7_no_100">
+  <h2 class="home_tagline">Fine art you will cherish forever. <span>Trust your instincts.</span><span>Trust yourself. </span></h2></div>
+<div style="display:block; width: 100%; padding-top: 10px;">
+<div class="breadcrumb" style="margin-top: 10px;"><a style="display:block; width: 100%;" href="Artists--Bios.html" class="content_btn home_btn_small"><span class="btn_text_break">click now to find a painting</span> <span class="btn_text_break">that "speaks to you"</span></a></div>
+<div class="breadcrumb" style="margin-top: 10px;"><a style="display:block; width: 100%;" href="art_lovers_niche.htm" class="content_btn home_btn_small"><span class="btn_text_break">Join us at </span> Art Lovers' Niche. <span class="btn_text_break">Don't miss out!</span></a></div>
+<div class="breadcrumb" style="margin-top: 10px;"><a style="display:block; width: 100%;" href="tel:1-724-459-0612" class="content_btn home_btn_small"><span class="btn_text_break">Call us anytime:  724-459-0612</span></a></div>
+</div>
+
+</div>
+<div class="flex_6">
+
+<div style="max-width:550px; margin:auto;">
+<div class="video-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/9-yObV8vpLc?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div>
+
+
+</div>
+</div>
+</section>
+</div>
+<div class="container secondary" style="padding-top: 24px;">
+<section class="wrapper" style="max-width: 860px; margin:auto;">
+
+<h2 class="reverse_header" style="margin-top: 0px;" id="testimonialsAnchor">Our Customers are the best! There is a reason over 40,000 people flock to our website annually to find fine art they love - It's because we earn your trust. Here's what our customers have to say:</h2>
+
+<div class="home_test">
+
+<TestimonialsScroll />
+</div>
+</section>
+</div>
+
+
+
+
+<div class="container primary" style="padding-top: 0px;">
+<section class="wrapper">
+
+<div class="home_sales">
+<p class="home_thumbnails" style="color: rgba(16,88,185,1.00); text-align:center;">Bedford Fine Art Gallery is your best resource for finding paintings with that "wow factor", that are the focal point in any room. The best testimonials to the fine art people have fallen in love with are our notable sales to many happy customers throughout the United States and in select locations around the world. </p>
+
+<h2 class="reverse_header" style="margin-top: 0px; color: rgba(16,88,185,1.00);" id="testimonialsAnchor">Some notable sales to many happy customers</h2>
+
+</div>
+
+   <div style=" border-bottom: 1px solid #dddd85; padding-bottom:20px; margin-bottom:20px;">
+<ul class="productGrid2" style="max-width:1260px; margin:0px auto 0px auto; padding-top: 10px;">
+
+<li><div class="sold"><span class="soldTag">sold</span><a href="octavius_white_niagra.html"><nuxt-img src="IMG_4142_grid.jpg" alt="Octavius White (19th Century Canadian School)
+"/>
+<p class="artist_gallery_title">Octavius White (19th Century Canadian School)</p>
+
+
+</a></div></li>
+
+</ul>
+   </div>
+
+  <div class="breadcrumb"><a href="Artists--Bios.html">click to view over 250 unique paintings for sale now</a></div>
+</section>
+</div>
+
+
+<div class="container secondary" style="padding-bottom:24px;">
+<section class="wrapper clearfix" style="max-width: 860px; margin:auto;">
+<h2 class="reverse_header" style="margin-top: 0px;" id="testimonialsAnchor">For over 30 years we have had the great pleasure to work with leaders who share our passion for fine art - here are some of the kind words they had to say:</h2>
+
+  <nuxt-img style="width: 100%; height:auto; max-width: 150px; margin:auto; display:block; padding: 20px 0px;" src="star_reviews.png" width="1566" height="283" alt=""/>
+   <div>
+     <p class="more" style="text-align:left;">"I first met Joan and Jerry through a recommendation from my sister to go see their wonderful collection. Once I started to talk with them, I could see how much they loved learning about the psychology of art like me! I had the privilege to speak about Dialectical Behavioral Therapy art viewing and how the brain works when you view art. Art creates a safe place to bring forth emotions and Bedford Fine Art Gallery provided a relaxed environment, helping people find the art they love."</p><span style="display: block; font-weight:bold; color: #f2f2f2; padding-top: 10px;">Dr. Terri Nicole Sawyer, LCSW, CFLE + C-DBT</span></div>
+
+  <nuxt-img style="width: 100%; height:auto; max-width: 150px; margin:auto; display:block; padding: 20px 0px;" src="star_reviews.png" width="1566" height="283" alt=""/>
+   <div><p class="more" style="text-align:left;">"My interest in art conservation began as an undergraduate student at Boston University's College of Fine Arts, from which I earned my BFA in Painting and Printmaking. This experience led me to entry into the three year graduate program in Painting Conservation sponsored by Dominican University ( Rosary College ), River Forest, IL conducted at the Villa Schifanoia, Florence Italy, from which I earned my MFA in Painting Conservation. I am approaching my fiftieth year as a practitioner and for at least half of that time, it has been my pleasure to include in my clientele, Joan and Jerry Hawk, proprietors of Bedford Fine Art Gallery, Bedford PA. They are conscientious and attentive to the requirements of caring for and maintaining art work in the most responsible manner and I see the attention applied to the interest of their prospective clients as well."</p><span style="display: block; font-weight:bold; color: #f2f2f2; padding-top: 10px;">Michael Max Mosorjak, MFA, BFA, Proprietor of Painting Conservation Laboratory</span></div>
+
+
+  <nuxt-img style="width: 100%; height:auto; max-width: 150px; margin:auto; display:block; padding: 20px 0px;" src="star_reviews.png" width="1566" height="283" alt=""/>
+   <div><p class="more" style="text-align:left;">"As the site director to the Southern Alleghenies Museum of Art in Bedford, I have worked with Jerry as the head of SAMA's Acquisitions Committee, Vice President of Development, and as an active board member. His unyielding passion for fine art is ever-present. Additionally, I have had the pleasure of working with his wife, Joan. Her exquisite taste in fine art has been a tremendous resource when working together to display selections from our permanent collection that numbers over 7,000 works from local, regional, national and international artists. They both have a sharp eye for detail, and together, Jerry and Joan have been continual key figures in the procuring and exhibiting of art for SAMA. I find their support and dedication to the organization to be significant, substantial, and detailed."</p><span style="display: block; font-weight:bold; color: #f2f2f2; padding-top: 10px;">Morgan Young, BFA, Bedford Site Director Southern Alleghenies Museum of Art.</span></div>
+
+
+  <nuxt-img style="width: 100%; height:auto; max-width: 150px; margin:auto; display:block; padding: 20px 0px;" src="star_reviews.png" width="1566" height="283" alt=""/>
+   <div><p class="more" style="text-align:left;">"I make it a point to visit other gallery's whenever I travel. Sadly, I'm usually disappointed by both the quality of the artwork shown and the presentation. I was very pleasantly surprised by the Bedford Gallery's Collection in both depth and quality. The gallery exhibited an interesting collection of 19th and early 20th Century artists supported  with meaningful biographical data. The exhibition spaces were complimentary and an unusual surprise in a quaint historic city. Far superior to most of the galleries that I've visited in most big American cities."</p><span style="display: block; font-weight:bold; color: #f2f2f2; padding-top: 10px;">Sam Berkovitz, Certified Appraiser, Appraisers Association of America.</span></div>
+
+
+  </section>
+</div>
+
+
+
+<div class="container primary" style="padding-bottom:24px;">
+<section class="wrapper clearfix as_seen_in_container">
+<nuxt-img class="home_logo" src="logo.png" width="123" height="124" style="width: 100%; height:auto;" alt="19th Century Paintings"/><span>As Seen In:</span>
+
+<div class="as_seen_in"><div>American Art Review</div>
+<div>Pittsburgh Quarterly</div>
+<div>Carnegie Magazine</div>
+<div>Johnstown Magazine</div>
+<div>Elan Magazine</div>
+<div>Allegany Magazine</div>
+<div>Bedford Gazette</div></div>
+</section>
+
+</div>
+
+
+
+<div class="container secondary_dk" style="padding-bottom:24px;">
+<section class="wrapper clearfix">
+
+<div class="flex_2">
+<div style="max-width:550px; margin:23px auto 0px auto;" id="start_vid">
+<h2 style="color: #742924; text-transform:capitalize; text-align:center; padding: 10px; font-size: 20px; font-weight: bold;">Dr. Terri Nicole Sawyer</h2
+><div class="video-container" style="margin: 0px auto 30px auto; width: 90%;"><iframe width="560" height="315" src="https://www.youtube.com/embed/cFAempT0P1Y?rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div>
+
+
+<div style="max-width:550px; margin:23px auto 0px auto;" id="video_anchor">
+<h2 style="color: #742924; text-transform:capitalize; text-align:center; padding: 10px; font-size: 20px; font-weight: bold;">Virtual Tour</h2
+><div class="video-container" style="margin: 0px auto 30px auto; width: 90%;" id="video_anchor"><iframe width="560" height="315" src="https://www.youtube.com/embed/iQuzsnwvbM8?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div>
+
+</div>
+
+
+
+<div class="breadcrumb"><a href="Artists--Bios.html" class="content_btn">show me a painting i will love</a></div>
+
+
+<h2 style="color: #742924; text-transform:capitalize; text-align:center; padding: 10px; font-size: 20px; font-weight: bold; padding-top: 40px;" class="secondary_dk_divider">Our Story</h2
+>
+<div style="max-width: 660px; margin:auto;">
+<div class="video-container2" style="clear:both;"><iframe width="560" height="315" src="https://www.youtube.com/embed/UlgTd5n_CEE?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></div>
+<p style="color:#742924; font-weight:bold; text-align:center; margin-top: 20px;"><span>Want to read our story?</span><a style="display:block; max-width:300px; background:rgba(16,88,185,1.00); color:#f2f2f2; text-decoration:none; padding: 12px; margin: auto; margin-top: 12px; text-align:center;" href="Gallery-Value.html">ABOUT THE GALLERY</a></p>
+</section>
+</div>
+
+
+<div class="container secondary_dk" style="padding-bottom:24px;">
+<section class="wrapper clearfix scondary_dk_divider">
+<div class="flex_wrap">
+<div class="flex_3"><div class="video-container"><iframe width="660" height="415" src="https://www.youtube.com/embed/5I8BEyiGICs?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div>
+<div class="flex_3"><div class="video-container"><iframe width="660" height="415" src="https://www.youtube.com/embed/3asJzvyy-ZQ?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div>
+<div class="flex_3"><div class="video-container"><iframe width="660" height="415" src="https://www.youtube.com/embed/o6PhOHhaA5M?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div>
+</div>
+
+
+
+
+
+
+
+<div class="breadcrumb"><a href="Artists.html" class="content_btn">view over 250 well listed artists</a></div>
+
+</section>
+</div>
+
+
+
+
+
+
+</div>
 </template>
 
-<script>
-export default {
-    name: 'IndexPage',
-}
-</script>
+
+
