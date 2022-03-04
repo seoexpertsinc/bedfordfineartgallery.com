f202d87a7c1d87500180a41d45aab08a6f3f72f1
Shawn Schmalz <shawns@nittanyweb.com>
2022-03-04 18:34:04 +0000

    updates

---
+++ components/Gallery-new.vue
@@ -0,0 +1,48 @@
+<template>
+    <div style="display: inline;">
+
+<li><a href="charles_henry_eaton_apple.html"><nuxt-img src="eaton_apple_orchard_grid.jpg" alt="Charles Henry Eaton (American 1850 – 1901)
+"/>
+<p class="artist_gallery_title"><span class="gallery_new_label label">NEW</span>Charles Henry Eaton (American 1850 – 1901)</p>
+
+
+</a></li>
+
+<li><a href="allen_blagden_migration.html"><nuxt-img src="blagden_migration_grid.jpg" alt="Allen Blagden (American, b. 1938)
+"/>
+
+<p class="artist_gallery_title"><span class="gallery_new_label label">NEW</span>Allen Blagden (American, b. 1938)</p>
+
+</a></li>
+
+<li><a href="blendon_reed_campbell_hillside.html"><nuxt-img src="campbell_hillside_grid.jpg" alt="Blendon Reed Campbell (American 1872 - 1969)
+"/>
+<p class="artist_gallery_title"><span class="gallery_new_label label">NEW</span>Blendon Reed Campbell (American 1872 - 1969) </p>
+
+
+</a></li>
+
+
+<li><a href="ernest_fredericks_winter.html"><nuxt-img src="fredericks_winter_grid.jpg" alt="Ernest Fredericks (1877 - 1959)
+"/>
+<p class="artist_gallery_title"><span class="gallery_new_label label">NEW</span>Ernest Fredericks (1877 - 1959) </p>
+
+
+</a></li>
+    </div>
+</template>
+
+<style scoped>
+.gallery_new_label {
+  padding: 5px;
+  display: inline-block;
+  margin-right: 5px;
+  border-radius: 2px;
+  background: #B90D0F;
+  color: #FFFFFF;
+  font-size: 12px;
+  line-height: 12px;
+  text-decoration: none;
+  font-weight: normal;
+}
+</style>
---
+++ pages/artists-bios.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+
+<div class="container secondary">
+<section class="wrapper clearfix">
+<div class="pagination_sub">
+<ul><li><a class="active" href="Artists--Bios.html">All</a></li> <li><a href="landscape_artwork.html">Landscape</a></li>
+<li><a href="marine_artwork.html">Marine</a></li>
+<li><a href="still_life_artwork.html">Still Life</a></li>
+<li><a href="genre_artwork.html">Genre</a></li>
+<li><a href="historical_and_political_artwork.html">American Historic / Political</a></li>
+<li><a href="sporting_art.html">Sporting Art</a></li>
+<li><a href="animal_artwork.html">Animal</a></li>
+<li><a href="sculpture.html">Sculpture</a></li>
+<li><a href="notable_sales.html">Recently Sold</a></li>
+</ul></div>
+
+<ul class="productGrid2" id="myUL" style="padding-top: 15px;">
+<GalleryNew />
+<GalleryReg />
+
+</ul>
+</section></div>
+
+
+</div>
+</template>
+
+<style scoped>
+.secondary {
+  background-image: none;
+  background-repeat: repeat;
+  background: #222;
+}
+
+
+</style>
