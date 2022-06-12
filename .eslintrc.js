733c944cec91a6335c2f751c6d990c090173a901
Adam Hopkins <violapiratejunky@gmail.com>
2022-06-12 18:07:36 -0500

    fix spacing

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,5 @@ module.exports = {
     extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
     plugins: [],
     // add your custom rules here
-    rules: {'vue/multi-word-component-names': 'off'},
+    rules: { 'vue/multi-word-component-names': 'off' },
 }
