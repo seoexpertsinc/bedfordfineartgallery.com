7432b29612ef4112b3b49aa143a1ec3e6fe9ec19
Shawn Schmalz <shawns@nittanyweb.com>
2022-09-22 16:44:08 +0000

    video pages linked to in previous mailers

---
+++ pages/videos.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+<div class="container primary">
+<section class="wrapper clearfix">
+<div class="artwork_header">
+  <h1>Different by Design</h1>
+  <span class="hr"></span></div>
+
+
+<div>
+
+<div style="max-width:750px; margin:auto;">
+<div class="video-container">
+<YouTubeVideo link="https://www.youtube.com/embed/9-yObV8vpLc?rel=0" />
+
+</div>
+
+
+<div class="breadcrumb"><a href="Artists--Bios.html" class="content_btn">view our gallery</a></div>
+
+</div>
+</div>
+
+</section>
+</div>
+    </div>
+</template>
+
+<style>
+.y-video__media--type--img {
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+</style>
+
+<router>
+  {
+    path: '/videos.html',
+  }
+</router>
---
+++ pages/view_video.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+<div class="container primary">
+<section class="wrapper clearfix">
+<div class="artwork_header">
+  <h1>Dr. Terri Nicole Sawyer</h1>
+  <span class="hr"></span></div>
+
+
+<div>
+
+<div style="max-width:750px; margin:auto;">
+<div class="video-container">
+<YouTubeVideo link="https://www.youtube.com/embed/cFAempT0P1Y?rel=0" />
+
+</div>
+
+
+<div class="breadcrumb"><a href="Artists--Bios.html" class="content_btn">view our gallery</a></div>
+
+</div>
+</div>
+
+</section>
+</div>
+    </div>
+</template>
+
+<style>
+.y-video__media--type--img {
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+</style>
+
+<router>
+  {
+    path: '/view_video.html',
+  }
+</router>
