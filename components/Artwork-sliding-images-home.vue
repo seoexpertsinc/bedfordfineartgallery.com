2d40bc531b0ed53618aa1ae5d9a4cfc84fe67425
Adam Hopkins <violapiratejunky@gmail.com>
2022-09-23 01:18:41 -0500

    format

--- components/Artwork-sliding-images-home.vue
+++ components/Artwork-sliding-images-home.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
         <VueSlickCarousel v-bind="settings">
-            <nuxt-link v-for="scrollingHomepageImage in scrollingHomepageImages" :key="scrollingHomepageImage.slug" :to="scrollingHomepageImage.slug.replace('-html', '.html')"
-                ><nuxt-img :src="getImage(scrollingHomepageImage)" width="290" height="200" :alt="scrollingHomepageImage.title"
+            <nuxt-link
+                v-for="scrollingHomepageImage in scrollingHomepageImages"
+                :key="scrollingHomepageImage.slug"
+                :to="scrollingHomepageImage.slug.replace('-html', '.html')"
+                ><nuxt-img
+                    :src="getImage(scrollingHomepageImage)"
+                    width="290"
+                    height="200"
+                    :alt="scrollingHomepageImage.title"
             /></nuxt-link>
         </VueSlickCarousel>
     </div>
@@ -17,10 +24,10 @@ import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
     components: { VueSlickCarousel },
     props: {
-      scrollingHomepageImages: {
-        type: Array,
-        required: true,
-      },
+        scrollingHomepageImages: {
+            type: Array,
+            required: true,
+        },
     },
     data() {
         return {
@@ -34,39 +41,41 @@ export default {
                 speed: 9000,
                 autoplaySpeed: 2000,
                 cssEase: 'linear',
-                  "responsive": [
-    {
-      "breakpoint": 1024,
-      "settings": {
-        "slidesToShow": 3,
-        "slidesToScroll": 3,
-        "infinite": true,
-        "dots": true
-      }
-    },
-    {
-      "breakpoint": 600,
-      "settings": {
-        "slidesToShow": 2,
-        "slidesToScroll": 2,
-        "initialSlide": 2
-      }
-    },
-    {
-      "breakpoint": 480,
-      "settings": {
-        "slidesToShow": 1,
-        "slidesToScroll": 1
-      }
-    }
-  ]
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true,
+                        },
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            initialSlide: 2,
+                        },
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        },
+                    },
+                ],
             },
         }
     },
     methods: {
-      getImage(scrollingHomepageImage) {
-        return scrollingHomepageImage.gridImage ? scrollingHomepageImage.gridImage : scrollingHomepageImage.mediumResImage
-      }
-    }
+        getImage(scrollingHomepageImage) {
+            return scrollingHomepageImage.gridImage
+                ? scrollingHomepageImage.gridImage
+                : scrollingHomepageImage.mediumResImage
+        },
+    },
 }
 </script>
